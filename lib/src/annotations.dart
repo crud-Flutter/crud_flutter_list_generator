d692fa5126e9bef4cccd19192f3f3355222aa32d
Lucas Garcia de Araujo Martins <lukasgarcya@hotmail.com>
2019-12-17 21:10:34 -0300

    Annotation for list flutter

---
+++ lib/src/annotations.dart
@@ -0,0 +1,12 @@
+class ListEntity {
+  final String titlePage;
+  const ListEntity({this.titlePage});
+}
+
+class ListTitle {
+  const ListTitle();
+}
+
+class ListSubTitle {
+  const ListSubTitle();
+}
