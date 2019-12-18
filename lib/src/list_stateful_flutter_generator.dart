750d1dcd868c7e04f968ac19c8980f270a5f6f28
Lucas Garcia de Araújo Martins <lucas.araujo@ifgoiano.edu.br>
2019-12-18 13:51:53 -0300

    class for generation in this file

--- lib/src/list_stateful_flutter_generator.dart
+++ lib/src/list_stateful_flutter_generator.dart
@@ -3,7 +3,7 @@ import 'dart:async';
 import 'package:analyzer/dart/element/element.dart';
 import 'package:build/src/builder/build_step.dart';
 import 'package:crud_flutter_list_generator/crud_flutter_list_generator.dart';
-import 'package:crud_flutter_list_generator/src/class_source.dart';
+import 'package:crud_generator/crud_generator.dart';
 import 'package:source_gen/source_gen.dart';
 
 class ListStatefulFlutterGenerator extends GeneratorForAnnotation<ListEntity> {
@@ -12,4 +12,15 @@ class ListStatefulFlutterGenerator extends GeneratorForAnnotation<ListEntity> {
     return GenerateListStatefulFlutterClass(element.name).build();
   }
 
+}
+
+class GenerateListStatefulFlutterClass extends GenerateFlutterWidgetAbstract {
+  GenerateListStatefulFlutterClass(String name)
+      : super(name, classSuffix: 'ListFulPage', parentClass: 'StatefulWidget');
+  @override
+  void generateWidget() {
+    generateClass.writeln(classPrefix +
+        'PageState createState() => $classPrefix' +
+        'PageState();');
+  }
 }
--- lib/src/list_stateless_flutter_generator.dart
+++ lib/src/list_stateless_flutter_generator.dart
@@ -3,10 +3,9 @@ import 'dart:async';
 import 'package:analyzer/dart/element/element.dart';
 import 'package:build/src/builder/build_step.dart';
 import 'package:crud_flutter_list_generator/crud_flutter_list_generator.dart';
+import 'package:crud_generator/crud_generator.dart';
 import 'package:source_gen/source_gen.dart';
 import 'package:source_gen/src/constants/reader.dart';
-
-import 'class_source.dart';
 
 class ListStatelessFlutterGenerator extends GeneratorForAnnotation<ListEntity> {
   @override
@@ -23,5 +22,44 @@ class ListStatelessFlutterGenerator extends GeneratorForAnnotation<ListEntity> {
     } catch (FormatException) {
       return element.name + 's';
     }
+  }
+}
+
+class GenerateListStatelessFlutterClass extends GenerateFlutterWidgetAbstract {
+  String titlePage;
+  String listTitle;
+  String listSubTitle;
+
+  GenerateListStatelessFlutterClass(String name)
+      : super(name, classSuffix: 'ListPage', parentClass: 'StatelessWidget');
+
+  GenerateListStatelessFlutterClass setTitlePage(String titlePage) {
+    this.titlePage = titlePage;
+    return this;
+  }
+
+  GenerateListStatelessFlutterClass setListTitle(String title) {
+    listTitle = title;
+    return this;
+  }
+
+  GenerateListStatelessFlutterClass setListSubTitle(String title) {
+    listSubTitle = title;
+    return this;
+  }
+
+  @override
+  void generateWidget() {
+    generateClass.writeln('Widget build(BuildContext context) {');
+    generateClass.writeln('return Scaffold(');
+    generateClass.writeln('appBar: AppBar(');
+    generateClass.writeln('title: Text(\'$titlePage\'),');
+    generateClass.writeln('),');
+    generateClass.writeln('body: Container(),');
+    generateClass.writeln('floatingActionButton: FloatingActionButton(');
+    generateClass.writeln('child: Icon(Icons.add),');
+    generateClass.writeln('),');
+    generateClass.writeln(');');
+    generateClass.writeln('}');
   }
 }
