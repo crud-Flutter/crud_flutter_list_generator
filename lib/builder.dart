bc4de37eddaf04afeaefa6c1f06f14ecaa44b5e3
Lucas Garcia de Araujo Martins <lukasgarcya@hotmail.com>
2019-12-18 12:10:42 -0300

    builder for stateful list

--- lib/builder.dart
+++ lib/builder.dart
@@ -1,6 +1,12 @@
 import 'package:build/build.dart';
 import 'package:source_gen/source_gen.dart';
 
-import 'src/list_flutter_generator.dart';
+import 'src/list_stateless_flutter_generator.dart';
+import 'src/list_stateful_flutter_generator.dart';
 
-Builder listStatelessFlutterBuilder(BuilderOptions options) => LibraryBuilder(ListStatelessFlutterGenerator(), generatedExtension: '.list.stateless.dart');
+Builder listStatelessFlutterBuilder(BuilderOptions options) =>
+    LibraryBuilder(ListStatelessFlutterGenerator(),
+        generatedExtension: '.list.stateless.dart');
+Builder listStateFulFlutterBuilder(BuilderOptions options) =>
+    LibraryBuilder(ListStatefulFlutterGenerator(),
+        generatedExtension: '.list.stateful.dart');
