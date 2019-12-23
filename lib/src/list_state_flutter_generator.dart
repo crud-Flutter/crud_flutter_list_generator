0f00d64ee879377c2daeddc4beaa14a9c3a74785
Lucas Garcia de Araujo Martins <lukasgarcya@hotmail.com>
2019-12-23 14:01:01 -0300

    list state use code_builder

--- lib/src/list_state_flutter_generator.dart
+++ lib/src/list_state_flutter_generator.dart
@@ -1,75 +1,69 @@
+// import 'dart:async';
+
+// import 'package:analyzer/dart/element/element.dart';
+// import 'package:build/src/builder/build_step.dart';
+// import 'package:crud_flutter_list_generator/crud_flutter_list_generator.dart';
+// import 'package:crud_generator/crud_generator.dart';
+// import 'package:source_gen/source_gen.dart';
+
 import 'dart:async';
 
 import 'package:analyzer/dart/element/element.dart';
-import 'package:build/src/builder/build_step.dart';
+import 'package:build/build.dart';
+import 'package:code_builder/code_builder.dart';
 import 'package:crud_flutter_list_generator/crud_flutter_list_generator.dart';
 import 'package:crud_generator/crud_generator.dart';
 import 'package:source_gen/source_gen.dart';
 
-class ListStateFlutterBuilder extends GeneratorForAnnotation<ListEntity> {
+class ListStateFlutterBuilder
+    extends GenerateFlutterWidgetForAnnotation<ListEntity> {
   @override
   FutureOr<String> generateForAnnotatedElement(
       Element element, ConstantReader annotation, BuildStep buildStep) {
-    return GenerateListStateFlutterClass(element.name)
-        .setListTitle(getValueAnnotation(element, annotation, 'listTitle'))
-        .setListSubTitle(
-            getValueAnnotation(element, annotation, 'listSubTitle'))
-        .build();
-  }
-}
-
-class GenerateListStateFlutterClass extends GenerateFlutterWidgetAbstract {
-  String listTitle;
-  String listSubTitle;
-  GenerateListStateFlutterClass(String name)
-      : super(name,
-            classSuffix: 'ListPageState',
-            parentClass: 'State<$name' 'ListFulPage>') {
-    generateClass.writeln('final _bloc = $classPrefix' 'Bloc();');
+    name = '${element.name}ListFulPageState';
+    extend = refer('State<${element.name}ListFulPage>');
+    this.element = element;
+    this.annotation = annotation;
+    _declareField();
+    _methodBuild();
+    return "import '${element.name.toLowerCase()}.list.stateful.dart';"
+            "import '${element.name.toLowerCase()}.bloc.dart';" +
+        build();
   }
 
-  GenerateListStateFlutterClass setListTitle(String listTitle) {
-    this.listTitle = listTitle;
-    return this;
+  void _declareField() {
+    declareField(refer('${element.name}Bloc'), '_bloc',
+        assignment: Code('${element.name}Bloc()'));
   }
 
-  GenerateListStateFlutterClass setListSubTitle(String listSubTitle) {
-    this.listSubTitle = listSubTitle;
-    return this;
-  }
-
-  @override
-  void generateWidget() {
-    generateClass.writeln('Widget build(BuildContext context) {');
-    generateClass.writeln('return StreamBuilder<List<$entityClass>>(');
-    generateClass.writeln('stream: _bloc.$nameLowerCase' 's,');
-    generateClass.writeln('builder: (context, snapshot) {');
-    generateClass
-        .writeln('if (!snapshot.hasData) return Center(child: CircularProgressIndicator());');
-    // generateClass.writeln(
-    //     'if (snapshot.hasError) return SimpleDilaog(title: snapshot.error);');
-    generateClass.writeln('return ListView(');
-    generateClass.writeln('children: snapshot.data.map(($entityInstance) {');
-    generateClass.writeln('return ListTile(');
-    if (listTitle != null) {
-      generateClass.writeln('title: Text($entityInstance.$listTitle),');
-    }
-    if (listSubTitle != null) {
-      generateClass.writeln('subtitle: Text($entityInstance.$listSubTitle),');
-    }
-    generateClass.writeln(');');
-    generateClass.writeln('}).toList()');
-    generateClass.writeln(');');
-    generateClass.writeln('},');
-    generateClass.writeln(');');
-    generateClass.writeln('}');
-  }
-
-  @override
-  void addImports() {
-    super.addImports();
-    importEntity();
-    importGenerate('list.stateful');
-    importGenerate('bloc');
+  void _methodBuild() {
+    var buildCode = [
+      Code('return StreamBuilder<List<$entityClass>>('),
+      Code('stream: _bloc.list,'),
+      Code('builder: (context, snapshot) {'),
+      Code(
+          'if (!snapshot.hasData) return Center(child: CircularProgressIndicator());'),
+      // Code(
+      //     'if (snapshot.hasError) return SimpleDilaog(title: snapshot.error);'),
+      Code('return ListView('),
+      Code('children: snapshot.data.map(($entityInstance) {'),
+      Code('return ListTile(')
+    ];
+    try {
+      buildCode.add(Code(
+          'title: Text($entityInstance.${getAnnotationValue("listTitle").stringValue}),'));
+    } catch (e) {}
+    try {
+      buildCode.add(Code(
+          'subtitle: Text($entityInstance.${getAnnotationValue("listSubTitle").stringValue}),'));
+    } catch (e) {}
+    buildCode.add(Code(');'));
+    buildCode.add(Code('}).toList()'));
+    buildCode.add(Code(');'));
+    buildCode.add(Code('},'));
+    buildCode.add(Code(');'));
+    var blockBuilder = BlockBuilder();
+    blockBuilder.statements.addAll(buildCode);
+    methodBuild(blockBuilder.build());
   }
 }
