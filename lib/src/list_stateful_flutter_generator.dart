2f42df4629241ae5cce7a83b5851aad3048ea637
Lucas Garcia de Araujo Martins <lukasgarcya@hotmail.com>
2019-12-23 14:00:44 -0300

    LIstStateful usage code_builder

--- lib/src/list_stateful_flutter_generator.dart
+++ lib/src/list_stateful_flutter_generator.dart
@@ -1,32 +1,29 @@
 import 'dart:async';
 
 import 'package:analyzer/dart/element/element.dart';
-import 'package:build/src/builder/build_step.dart';
+import 'package:build/build.dart';
+import 'package:code_builder/code_builder.dart';
 import 'package:crud_flutter_list_generator/crud_flutter_list_generator.dart';
 import 'package:crud_generator/crud_generator.dart';
 import 'package:source_gen/source_gen.dart';
 
-class ListStatefulFlutterGenerator extends GeneratorForAnnotation<ListEntity> {
+class ListStatefulFlutterGenerator
+    extends GenerateFlutterWidgetForAnnotation<ListEntity> {
   @override
-  FutureOr<String> generateForAnnotatedElement(Element element, ConstantReader annotation, BuildStep buildStep) {
-    return GenerateListStatefulFlutterClass(element.name).build();
+  FutureOr<String> generateForAnnotatedElement(
+      Element element, ConstantReader annotation, BuildStep buildStep) {
+    name = '${element.name}ListFulPage';
+    this.element = element;
+    this.annotation = annotation;
+    extend = refer('StatefulWidget');
+    _methodCreateState();
+    return "import '${element.name.toLowerCase()}.list.state.dart';" + build();
   }
 
-}
-
-class GenerateListStatefulFlutterClass extends GenerateFlutterWidgetAbstract {
-  GenerateListStatefulFlutterClass(String name)
-      : super(name, classSuffix: 'ListFulPage', parentClass: 'StatefulWidget');
-  @override
-  void generateWidget() {
-    generateClass.writeln(classPrefix +
-        'ListPageState createState() => $classPrefix' +
-        'ListPageState();');
-  }
-
-  @override
-  void addImports() {    
-    super.addImports();
-    importGenerate('list.state');
+  void _methodCreateState() {
+    declareMethod('createState',
+        returns: refer('${element.name}ListFulPageState'),
+        lambda: true,
+        body: Code('${element.name}ListFulPageState()'));
   }
 }
